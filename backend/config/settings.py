bd86efb22d2b2d4890b2154eeead9a3d08593407
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-04 16:02:07 -0700

    Create accounts app & CustomUser

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     # Local
-    "todos",
+    "accounts.apps.AccountsConfig",
+    "todos.apps.TodosConfig",
 ]
 
 MIDDLEWARE = [
@@ -98,8 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "America/Vancouver"
@@ -112,14 +111,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = "/static/"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
