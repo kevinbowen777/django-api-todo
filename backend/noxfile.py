5b9db4b9c70959e7318f4085e5b38bfa06a9effa
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-04 16:38:16 -0700

    Add factory-boy & accounts tests

--- backend/noxfile.py
+++ backend/noxfile.py
@@ -4,7 +4,7 @@ import tempfile
 import nox
 
 nox.options.sessions = "lint", "safety", "tests"
-locations = "config", "todos", "./noxfile.py"
+locations = "accounts", "config", "todos", "./noxfile.py"
 
 
 def install_with_constraints(session, *args, **kwargs):
