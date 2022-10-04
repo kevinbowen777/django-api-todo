af5546f33cded99cd63c0192194bee00350cd468
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-04 07:56:20 -0700

    Add django-extensions. Resolves #56

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # 3rd party
+    "django_extensions",
     "rest_framework",
     "corsheaders",
     # Local
