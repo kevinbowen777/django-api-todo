02cb5c0643fa4bc169ce629cda806aa8bd7af667
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-05 11:23:55 -0700

    Add react-router-dom & bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
