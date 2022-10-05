f73a17cda583bfb9f390d62e42483d46dc482df4
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-04 17:26:55 -0700

    Add created, completed, user fields

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField()
+    body = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
